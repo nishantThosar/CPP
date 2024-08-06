1949a628419dc82bfa241bd53ecd20bdfa94c9c5
System_32 <162736264+nishantThosar@users.noreply.github.com>
2024-08-06 23:59:10 +0530

    Laptop
    This pointer basics in CPP.
    This pointer is a special type of pointer which points to the current class.

---
+++ This_pointer/this_poointer_example.cpp
@@ -0,0 +1,46 @@
+/*This pointer*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class teacher
+{
+private:
+	double salary;
+public:
+	string name;
+	string subject;
+	string department;
+
+//parameterized constructor (in this constructor we will be using "this" pointer)
+	teacher(string name, string subject, string department, double salary=1500)
+	{
+		this->name = name;
+		this->subject = subject;
+		this->department = department;
+		this->salary = salary;
+	}//para constructor ends
+
+//class method
+	void display()
+	{
+		cout<<name<<endl;
+		cout<<department<<endl;
+		cout<<subject<<endl;
+		cout<<salary<<endl;
+		cout<<"\n"<<endl;
+	}//method ends
+	
+};// class ends
+
+int main(void)
+{
+	teacher Teacher_1("Kaushal Sharma", "DSA", "DESD");
+	Teacher_1.display(); //calling function(Method) of teacher class with reference object(Teacher_1).
+	
+	teacher Teacher_2("Mukul Sharma", "COSA", "DITTIS", 16230);
+	Teacher_2.display();
+
+	return 0;
+}
